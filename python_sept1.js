d81e838c976e2405ad30d69ec74534037d661e94
joshgressman <joshgressman@gmail.com>
2016-09-01 13:34:51 -0500

    python file if

---
+++ python_sept1.js
@@ -0,0 +1,20 @@
+Sept 2016 python syntax
+
+if statements
+
+if 5 > 3:
+//do something
+
+if 5 > 3:
+print("number is greater than five")
+
+now use a for loop with a if statement conditional
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+for number in numbers:
+if number > 5:
+print(number)
+
+This will print only numbers greater than five. If the statement is true,
+the function will run, if FALSE the function will do nothing
